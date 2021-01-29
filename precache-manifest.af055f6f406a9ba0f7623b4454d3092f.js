97b8684a0ea80194f324cf71fa6ca69da86bee76
Richard Brinkley <brinkley908@gmail.co.uk>
2021-01-29 09:09:39 +0000

    Updates

---
+++ precache-manifest.af055f6f406a9ba0f7623b4454d3092f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6e0d43563d0c2a6560310e3659eed1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b58fec65958f2b59e63d",
+    "url": "./static/css/main.3c149fa8.chunk.css"
+  },
+  {
+    "revision": "0b308553359635518131",
+    "url": "./static/js/2.f5d8b4b3.chunk.js"
+  },
+  {
+    "revision": "61bbd154541afeec6fe39020619acd9d",
+    "url": "./static/js/2.f5d8b4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b58fec65958f2b59e63d",
+    "url": "./static/js/main.e26fa6a4.chunk.js"
+  },
+  {
+    "revision": "dc70b17f0a3db046ef8b",
+    "url": "./static/js/runtime-main.3ab93de3.js"
+  }
+]);
